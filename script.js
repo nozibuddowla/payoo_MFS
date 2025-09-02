58883bf00973e00f3e2f13fa6ebfddb66c481e4a
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-09-02 18:12:57 +0600

    add login functioanlity and redirect to home

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("f society");
+
+// login button functionality
+document.getElementById("loginButton").addEventListener("click", function (event) {
+    event.preventDefault();
+    let mobileNumber = 1922438860;
+    let pinNumber = 1234;
+
+    let mobileNumberValue = document.getElementById("mobile-number").value;
+    let mobileNumberValueInt = parseInt(mobileNumberValue);
+
+    let pinNumberValue = document.getElementById("pin-number").value;
+    let pinNumberValueInt = parseInt(pinNumberValue);
+
+    if (mobileNumber === mobileNumberValueInt && pinNumber === pinNumberValueInt) {
+        window.location.href="./main.html"
+    } else {
+        alert(`invalid credentials`);
+        
+    }
+})

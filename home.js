4024a8e914b2db27f624a24cc8c039fe21208082
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-09-03 16:36:00 +0600

    implement account number and pin validation features

--- home.js
+++ home.js
@@ -1,15 +1,30 @@
 console.log("f society");
 
+let validPin = 1234;
+
 document.getElementById("add-money-btn").addEventListener("click", function (event) {
     event.preventDefault();
     let availableBalance = parseFloat(document.getElementById("available-balance").innerText)
     let bank = document.getElementById("bank").value;
-    let accountNumber = parseInt(document.getElementById("account-number").value);
+    let accountNumber = document.getElementById("account-number").value;
     let addAmount = parseFloat(document.getElementById("add-amount").value);
     let addPin = parseInt(document.getElementById("add-pin").value);
-    console.log(availableBalance);
-    
+
+    if (accountNumber.length !== 11) {
+        alert("please provide valid account number!");
+        return;
+    }
+
+    if (validPin !== addPin) {
+        alert("Please provide valid pin number!");
+        return
+    }
+
     let totalNewBalance = availableBalance + addAmount;
-    console.log(totalNewBalance);
     document.getElementById("available-balance").innerText = totalNewBalance;
+    document.getElementById("add-amount").value = "";
+    document.getElementById("bank").value = "";
+    document.getElementById("account-number").value = "";
+    document.getElementById("add-pin").value = "";
+    
 })

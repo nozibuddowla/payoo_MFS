33e5741aea7a5a8f74d07749c5137b2a8f252280
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-09-03 22:41:27 +0600

    implement cashOut feature

--- home.js
+++ home.js
@@ -2,6 +2,7 @@ console.log("f society");
 
 let validPin = 1234;
 
+// add money feature
 document.getElementById("add-money-btn").addEventListener("click", function (event) {
     event.preventDefault();
     let availableBalance = parseFloat(document.getElementById("available-balance").innerText)
@@ -27,4 +28,42 @@ document.getElementById("add-money-btn").addEventListener("click", function (eve
     document.getElementById("account-number").value = "";
     document.getElementById("add-pin").value = "";
     
+})
+
+
+// cash out feature
+document.getElementById("withdraw-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    let availableBalance = parseFloat(document.getElementById("available-balance").innerText);
+    let amount = parseFloat(document.getElementById("withdraw-amount").value);
+    
+
+    let accountNumber = document.getElementById("agent-number").value;
+    let pin = parseInt(document.getElementById("pin").value);
+    
+
+    if (accountNumber.length !== 11) {
+        alert("please provide valid account number!");
+        return;
+    }
+
+    if (validPin !== pin) {
+        alert("Please provide valid pin number!");
+        return;
+    }
+
+    let currentBalance = availableBalance - amount;
+    document.getElementById("available-balance").innerText = currentBalance;
+
+})
+
+// toggling feature
+document.getElementById("add-button").addEventListener("click", function () {
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("add-money-parent").style.display = "block";
+})
+
+document.getElementById("cash-out-button").addEventListener("click", function () {
+    document.getElementById("cash-out-parent").style.display = "block";
+    document.getElementById("add-money-parent").style.display = "none";
 })
